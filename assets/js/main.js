70f14201a3530984f665a9665b51037c0c191d42
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2021-10-16 14:56:35 +0100

    added comments to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
+/* current scene will change depending on options chosen by the reader
+*  current scene is fed into the loadScene() function to update the text
+*  and options available to the reader
+*/
+
 let currentScene = "openingScene";
+
+/* script data in JSON format. The opening key of each block denotes the scene that
+*  will be rendered. the paragraph key is the text the the user will see in each scene
+*  option1 and option2 will render on each UI button.
+*  The result keys will be fed into the loadScene() function
+*/
 
 let script = {
     "openingScene":
@@ -38,10 +49,13 @@ let script = {
     }
 };
 
+/* UI elements are assigned to consts for ease of access */
+
 const paragraph = document.getElementById("paragraph");
 const buttonOption1 = document.getElementById("buttonOption1");
 const buttonOption2 = document.getElementById("buttonOption2");
 
+/* Buttons take the users choices and feed them into loadScene() */
 buttonOption1.addEventListener('click', () => {
     currentScene = script[currentScene].option1Result;
     loadScene(currentScene);
@@ -52,10 +66,12 @@ buttonOption2.addEventListener('click', () => {
     loadScene(currentScene);
 });
 
+/* Feed JSON data into the HTML elements for rendering */
 loadScene = (scene) => {
     paragraph.innerHTML = script[scene].paragraph;
     buttonOption1.innerHTML = script[scene].option1;
     buttonOption2.innerHTML = script[scene].option2;
 };
 
+/* runs on page load and each time user makes a choice */
 loadScene(currentScene);
