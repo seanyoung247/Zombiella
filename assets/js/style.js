34f6eda51426f24a4db8f66527f822f4ac7ab2d7
sam-timmins <84273109+sam-timmins@users.noreply.github.com>
2021-10-17 17:05:15 +0100

    Add pumpkin button and styled

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,18 @@
-console.log('Style sheet')
+
+/* Create grow and shrink effect on the pumpkin buttons */
+const pumpkinGrowRef = document.querySelector('.btn-pumpkin');
+
+
+const growPumpkin = () => {
+  pumpkinGrowRef.classList.remove('shrink')
+  pumpkinGrowRef.classList.add('grow');
+}
+
+const shrinkPumpkin = () => {
+  pumpkinGrowRef.classList.remove('grow')
+  pumpkinGrowRef.classList.add('shrink')
+}
+
+
+pumpkinGrowRef.addEventListener('mouseover', growPumpkin);
+pumpkinGrowRef.addEventListener('mouseout', shrinkPumpkin);
