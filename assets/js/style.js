960836d043812aa0fcfbf967a61b5558a714925e
sam-timmins <84273109+sam-timmins@users.noreply.github.com>
2021-10-17 21:05:49 +0100

    Change style to the main content

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,18 +1,32 @@
 
 /* Create grow and shrink effect on the pumpkin buttons */
-const pumpkinGrowRef = document.querySelectorAll('.btn-pumpkin');
+
+const pumpkinBtnOneRef = document.getElementById('buttonOption1')
+const pumpkinBtnTwoRef = document.getElementById('buttonOption2')
 
 
-const growPumpkin = () => {
-  pumpkinGrowRef.classList.remove('shrink');
-  pumpkinGrowRef.classList.add('grow');
+const growPumpkin1 = () => {
+  pumpkinBtnOneRef.classList.remove('shrink');
+  pumpkinBtnOneRef.classList.add('grow');
 }
 
-const shrinkPumpkin = () => {
-  pumpkinGrowRef.classList.remove('grow');
-  pumpkinGrowRef.classList.add('shrink');
+const shrinkPumpkin1 = () => {
+  pumpkinBtnOneRef.classList.remove('grow');
+  pumpkinBtnOneRef.classList.add('shrink');
 }
 
+const growPumpkin2 = () => {
+    pumpkinBtnTwoRef.classList.remove('shrink');
+    pumpkinBtnTwoRef.classList.add('grow');
+  }
+  
+  const shrinkPumpkin2 = () => {
+    pumpkinBtnTwoRef.classList.remove('grow');
+    pumpkinBtnTwoRef.classList.add('shrink');
+  }
 
-pumpkinGrowRef.addEventListener('mouseover', growPumpkin);
-pumpkinGrowRef.addEventListener('mouseout', shrinkPumpkin);
+
+pumpkinBtnOneRef.addEventListener('mouseover', growPumpkin1);
+pumpkinBtnOneRef.addEventListener('mouseout', shrinkPumpkin1);
+pumpkinBtnTwoRef.addEventListener('mouseover', growPumpkin2);
+pumpkinBtnTwoRef.addEventListener('mouseout', shrinkPumpkin2);
